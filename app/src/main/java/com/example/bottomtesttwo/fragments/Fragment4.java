41ee6b61c03b24e6bcc96d628e727bddca2bc5f3
Dream <582149912@qq.com>
2019-08-15 22:54:43 +0800

    修改Fragment4关联布局文件

--- app/src/main/java/com/example/bottomtesttwo/fragments/Fragment4.java
+++ app/src/main/java/com/example/bottomtesttwo/fragments/Fragment4.java
@@ -28,7 +28,7 @@ public class Fragment4 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_3, container, false);
+        return inflater.inflate(R.layout.fragment_4, container, false);
     }
 
 }
